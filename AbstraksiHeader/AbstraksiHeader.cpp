5911020c69bdcb8981f7b1f5b937606a381c63a5
Cyberfire <buayagoreng99@gmail.com>
2024-05-28 13:16:15 +0800

    AbstraksiHeader

---
+++ AbstraksiHeader/AbstraksiHeader.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h> // Perpustakaan cmath digunakan untuk fungsi matematika seperti pow
+using namespace std;
+
+int main()
+{
+	int x;
+
+	x = pow(2, 3); // Menghitung 2 pangkat 3
+
+	cout << x << endl; // Menampilkan hasilnya
+}
