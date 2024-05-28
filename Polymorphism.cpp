27f425cffb4f7dfd27fccffe818521c1fdfc97d0
Cyberfire <buayagoreng99@gmail.com>
2024-05-28 11:22:14 +0800

    Fungsi virtual murni, membuat kelas seseorang menjadi kelas abstrak

--- Polymorphism.cpp
+++ Polymorphism.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 using namespace std;
+
+class seseorang
+{
+public:
+    virtual void pesan() = 0; // Fungsi virtual murni, membuat seseorang menjadi kelas abstrak
+};
     int main()
 {
 }
