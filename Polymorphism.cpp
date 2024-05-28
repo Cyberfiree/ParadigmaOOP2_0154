b6aefa8790ed3c602b5dd05b657dffb917c5b389
Cyberfire <buayagoreng99@gmail.com>
2024-05-28 11:22:35 +0800

    membuat virtual fungsi biasa

--- Polymorphism.cpp
+++ Polymorphism.cpp
@@ -5,6 +5,13 @@ class seseorang
 {
 public:
     virtual void pesan() = 0; // Fungsi virtual murni, membuat seseorang menjadi kelas abstrak
+
+
+    // membuat virtual fungsi biasa
+    // virtual void pesan()
+    // {
+    //    cout << "Pesan dari seseorang" << endl;
+    // }
 };
     int main()
 {
