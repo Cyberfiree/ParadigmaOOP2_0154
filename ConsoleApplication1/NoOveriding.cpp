f30ebcde8a2f69d8ec5284d7c66c6e80cfe56043
Cyberfire <buayagoreng99@gmail.com>
2024-05-28 11:41:54 +0800

    membuat kelas dasar menggunakan akses modif public dan fungsi virtual yg bisa di override oleh kelas turunan

---
+++ ConsoleApplication1/NoOveriding.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+//class parent
+// tambahkan final sesudah nama class
+// untuk mencegah adanya overriding
+
+// Kelas dasar
+class baseClass
+{
+public:
+    virtual void perkenalan() // Fungsi virtual yang bisa dioverride oleh kelas turunan
+    {
+        cout << "Hallo saya function dari base class";
+    }
+};
+int main()
+{
+}
